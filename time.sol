59d7b69e67020017aef459664469109ed600e14f
riosolar <83927142+riosolar@users.noreply.github.com>
2022-10-19 22:05:52 +0000

    Update time.sol

--- time.sol
+++ time.sol
@@ -1,25 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-// ----------------------------------------------------------------------------
-//
-// Tested date range 1970/01/01 to 2345/12/31
-//
-// Conventions:
-// Unit      | Range         | Notes
-// :-------- |:-------------:|:-----
-// timestamp | >= 0          | Unix timestamp, number of seconds since 1970/01/01 00:00:00 UTC
-// year      | 1970 ... 2345 |
-// month     | 1 ... 12      |
-// day       | 1 ... 31      |
-// hour      | 0 ... 23      |
-// minute    | 0 ... 59      |
-// second    | 0 ... 59      |
-// dayOfWeek | 1 ... 7       | 1 = Monday, ..., 7 = Sunday
-//
-//
-
-library aliwenTimeLibrary {
+library AliwenTimeLibrary {
 
     uint constant SECONDS_PER_DAY = 24 * 60 * 60;
     uint constant SECONDS_PER_HOUR = 60 * 60;
